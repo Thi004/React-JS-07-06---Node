6a5aed614c13c969f7b259da237d8ff1f226dccd
Thi004 <phamthuymanthi@gmail.com>
2024-07-06 21:19:02 +0700

    gioi thieu ES6 Node Map

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('hello')
+
+// Node => Node js => React
+// Node la moi truong de chay js va cac du an Node Js
+// JS truoc day chay o dau? HTML
+// Node: co rat nhieu thu vien ho tro ( ngay trong Node , online)
+// cu phap: node + ten_file
+
+
+// ES6 : 1 phien ban cua JS
+// Arrow Function
+let f1 = () => {
+    console.log('Arrow Function!!!!')
+}
+f1();
+f1();
+f1();
+let f2 = a => {
+    console.log('A = ' + a)
+}
+f2(10);
+f2(20);
+ let f3 = a => a * a;
+ console.log(f3(5));
+ console.log(f3(6));
+
+ let f4 = (a,b) => {
+     b = 10;
+     return a + b;
+ }
+ console.log(f4(5,6));
---
+++ map.js
@@ -0,0 +1,8 @@
+let arr = [2, 3, 1, 5, 8, 2];
+let sum = 0
+arr.map(e => {
+    sum += e;
+})
+console.log(sum)
+
+// dung map de chay cac phan tu trong mang ma khong can dung for
